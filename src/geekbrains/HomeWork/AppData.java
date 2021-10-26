bed7a7cb558a05321b130028032fabd306a79484
SmileShort <86470906+SmileShort@users.noreply.github.com>
2021-10-26 13:53:28 +0500

    Initial commit

---
+++ src/geekbrains/HomeWork/AppData.java
@@ -0,0 +1,16 @@
+package geekbrains.HomeWork;
+
+public class AppData {
+
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    public AppData(String header, int i, int i1) {
+    }
+
+}
---
+++ src/geekbrains/HomeWork/Main.java
@@ -0,0 +1,30 @@
+package geekbrains.HomeWork;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        File file = new File("car.txt");
+        System.out.println(file.isFile());
+        System.out.println(file.exists());
+        AppData car1 = new AppData("sunny", 50, 2000);
+
+        int count;
+        ArrayList<AppData> arrayList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+            while ((count = bufferedReader.read()) != -1){
+                System.out.print((char)count);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
